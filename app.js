d5c21ed46dfa1b3a8d815ca015d6d50397a81a9d
Guilherme Quintão <guilherme0105@hotmail.com>
2022-01-05 20:45:47 -0300

    Configurando algumas mensagens de erros básicos.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const currencyOneEl = document.querySelector('[data-js="currency-one"]')
 const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
 
+const url = 'https://v6.exchangerate-api.com/v6/362ee49ee3ac59a944f0a5ab/latest/USD'
 
+const getErrormessage = errorType => ({
+    'unsupported-code' : 'Não teve suporte para o tipo de moeda solicitado.',
+    'malformed-request' : 'Alguma parte de sua solicitação não segue a estrutura.',
+    'invalid-key' : 'Chave API não e válida.',
+    'inactive-account' : 'Seu endereço de e-mail nao foi confirmado ou conta inativa',
+    'quota-reached' : 'Atingiu o número maximo de solicitações permitidas no seu plano.',
+})[errorType] || 'Não foi possível obter as informações'
+const fetchExchanedRate = async () => {
+    try {
+       const response = await fetch(url)
+       const exchangeRateData = await response.json()
 
-currencyOneEl.innerHTML = `<option>oi</option>`
+       if(exchangeRateData.result === 'error'){
+           throw new Error(getErrormessage(exchangeRateData['error-type']))                     
+       }
+      
+    } catch (err){
+        alert(err.message)    
+    }
+
+}
+
+fetchExchangeRate()
+
+const option = `<option>oi</option>`
 
 currencyOneEl.innerHTML = option
 currencyTwoEl.innerHTML = option
